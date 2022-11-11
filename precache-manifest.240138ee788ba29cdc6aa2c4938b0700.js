ddd96da6ee43b49cb94364140438093f371feb40
Marcelo Dutto <marcelodutto@hotmail.com>
2022-11-10 23:59:59 -0300

    Updates

---
+++ precache-manifest.240138ee788ba29cdc6aa2c4938b0700.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a12651f1f3516cd7cf9",
+    "url": "/coderhouse_reactJs_entregas/static/css/main.9f2e4a7d.chunk.css"
+  },
+  {
+    "revision": "2a12651f1f3516cd7cf9",
+    "url": "/coderhouse_reactJs_entregas/static/js/main.2a12651f.chunk.js"
+  },
+  {
+    "revision": "c0d850e1296c0a3903a2",
+    "url": "/coderhouse_reactJs_entregas/static/js/1.c0d850e1.chunk.js"
+  },
+  {
+    "revision": "23ee6aeb3dc7d4c65031",
+    "url": "/coderhouse_reactJs_entregas/static/js/2.23ee6aeb.chunk.js"
+  },
+  {
+    "revision": "3c69f069b78eb12cd673",
+    "url": "/coderhouse_reactJs_entregas/static/js/runtime~main.3c69f069.js"
+  },
+  {
+    "revision": "49f288d37273f4b9ed1ceab98812b024",
+    "url": "/coderhouse_reactJs_entregas/static/media/img-cocina-1.49f288d3.jpg"
+  },
+  {
+    "revision": "c3902e9966fc03e0eb219f47b2d30e2a",
+    "url": "/coderhouse_reactJs_entregas/static/media/escritorio-1.c3902e99.jpg"
+  },
+  {
+    "revision": "c5ea42272a5874d27299d377eda71ee9",
+    "url": "/coderhouse_reactJs_entregas/static/media/escritorio-2.c5ea4227.jpg"
+  },
+  {
+    "revision": "6d04c6dbe0f325543588ce9380e6c04e",
+    "url": "/coderhouse_reactJs_entregas/static/media/escritorio-3.6d04c6db.jpg"
+  },
+  {
+    "revision": "e4a16ca06ed393d7e804ba15d08f68f6",
+    "url": "/coderhouse_reactJs_entregas/static/media/escritorio-4.e4a16ca0.jpg"
+  },
+  {
+    "revision": "150e821890c1f843dd0f4878b5b9b509",
+    "url": "/coderhouse_reactJs_entregas/static/media/escritorio-5.150e8218.jpg"
+  },
+  {
+    "revision": "19e9b36712bd548198d01997b118a96e",
+    "url": "/coderhouse_reactJs_entregas/static/media/estanteria-1.19e9b367.jpg"
+  },
+  {
+    "revision": "32d6978e106b01c8c28982fdca88e9a1",
+    "url": "/coderhouse_reactJs_entregas/static/media/estanteria-2.32d6978e.jpg"
+  },
+  {
+    "revision": "6b66b44b482fac7272a77fb47492d01b",
+    "url": "/coderhouse_reactJs_entregas/static/media/estanteria-3.6b66b44b.jpg"
+  },
+  {
+    "revision": "d7e86c8fa77d7692904acff4624de0a3",
+    "url": "/coderhouse_reactJs_entregas/static/media/estanteria-4.d7e86c8f.jpg"
+  },
+  {
+    "revision": "484e3aaeba74d460a2de7e17619c46d4",
+    "url": "/coderhouse_reactJs_entregas/static/media/estanteria-5.484e3aae.jpg"
+  },
+  {
+    "revision": "30574a4d7e72570fb998bf9a58f59b37",
+    "url": "/coderhouse_reactJs_entregas/static/media/mesacomedor-1.30574a4d.jpg"
+  },
+  {
+    "revision": "431d96fd74e8ab5451f27e8d8252d8a7",
+    "url": "/coderhouse_reactJs_entregas/static/media/mesacomedor-2.431d96fd.jpg"
+  },
+  {
+    "revision": "96b228d9a9ed24262ec69d6155c3dc1a",
+    "url": "/coderhouse_reactJs_entregas/static/media/mesacomedor-3.96b228d9.jpg"
+  },
+  {
+    "revision": "fba0636a3456b3b699918fe6c979aa93",
+    "url": "/coderhouse_reactJs_entregas/static/media/mesaratona-1.fba0636a.jpg"
+  },
+  {
+    "revision": "e7dfac841e5e5eb84add4ef30e5da573",
+    "url": "/coderhouse_reactJs_entregas/static/media/mesaratona-2.e7dfac84.jpg"
+  },
+  {
+    "revision": "e1bbca9d27954c168bdee75902755da6",
+    "url": "/coderhouse_reactJs_entregas/static/media/mesaratona-3.e1bbca9d.jpg"
+  },
+  {
+    "revision": "bd17ea4c0c78457f507dc595af37ae8f",
+    "url": "/coderhouse_reactJs_entregas/static/media/mesaratona-4.bd17ea4c.jpg"
+  },
+  {
+    "revision": "71ad4eeb5c4079eec26cc1f7e98452df",
+    "url": "/coderhouse_reactJs_entregas/static/media/rack-1.71ad4eeb.jpg"
+  },
+  {
+    "revision": "290cc0903e5bb3dcdf41ba0ab480feea",
+    "url": "/coderhouse_reactJs_entregas/static/media/rack-2.290cc090.jpg"
+  },
+  {
+    "revision": "9e9ddce213abf3a3a14a36e0c0b1f4a7",
+    "url": "/coderhouse_reactJs_entregas/static/media/rack-3.9e9ddce2.jpg"
+  },
+  {
+    "revision": "8686347f3085bf04ca86d9bbfc93cd93",
+    "url": "/coderhouse_reactJs_entregas/static/media/rack-4.8686347f.jpg"
+  },
+  {
+    "revision": "9ef2a00907f631fd850d883418429561",
+    "url": "/coderhouse_reactJs_entregas/static/media/rack-5.9ef2a009.jpg"
+  },
+  {
+    "revision": "cceb96a8b57e868ea13b0e8003819994",
+    "url": "/coderhouse_reactJs_entregas/static/media/sillas-1.cceb96a8.jpg"
+  },
+  {
+    "revision": "ef076c84ab4a63c2ae2c7e93910589c3",
+    "url": "/coderhouse_reactJs_entregas/static/media/sillas-2.ef076c84.jpg"
+  },
+  {
+    "revision": "e2b4c0d2379cb4736d0c6d0583341f89",
+    "url": "/coderhouse_reactJs_entregas/static/media/sillas-3.e2b4c0d2.jpg"
+  },
+  {
+    "revision": "a21c027378a4133c0a8bb13b1afd4e22",
+    "url": "/coderhouse_reactJs_entregas/static/media/sillas-4.a21c0273.jpg"
+  },
+  {
+    "revision": "321b96ed708ddca67993e24a7089ecd1",
+    "url": "/coderhouse_reactJs_entregas/static/media/sillon-1.321b96ed.jpg"
+  },
+  {
+    "revision": "66b5ab477398b3399011b98ba141d3b2",
+    "url": "/coderhouse_reactJs_entregas/static/media/sillon-2.66b5ab47.jpg"
+  },
+  {
+    "revision": "edce7dabd989be232503bac01173999d",
+    "url": "/coderhouse_reactJs_entregas/static/media/sillon-3.edce7dab.jpg"
+  },
+  {
+    "revision": "63ed3060c987b039ae39e8989cf78342",
+    "url": "/coderhouse_reactJs_entregas/index.html"
+  }
+];
